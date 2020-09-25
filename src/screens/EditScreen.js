615631deb1c6e59188bc5fface836226c2fd68df
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 16:50:21 +0300

    Adding Initial Form Values

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -8,9 +8,15 @@ const EditScreen = ({ navigation }) => {
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam('id')
   );
-  console.log(navigation.getParam('id'));
 
-  return <BlogPostForm />;
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        console.log(title, content);
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({});
