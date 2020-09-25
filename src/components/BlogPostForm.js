1b58bb191f98a278e42e9e5474c4782f58944f11
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 16:58:05 +0300

    Adding Default Props

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const BlogPostForm = ({ onSubmit, initialValues }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState(initialValues.title);
+  const [content, setContent] = useState(initialValues.content);
 
   return (
     <View>
@@ -18,6 +18,13 @@ const BlogPostForm = ({ onSubmit, initialValues }) => {
       <Button title='Save Blog Post' onPress={() => onSubmit(title, content)} />
     </View>
   );
+};
+
+BlogPostForm.defaultProps = {
+  initialValues: {
+    title: '',
+    content: '',
+  },
 };
 
 const styles = StyleSheet.create({
