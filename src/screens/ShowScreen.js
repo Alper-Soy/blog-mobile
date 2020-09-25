c6df2306cd175c1bb8a5f75d00bee2b951e14e5b
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 16:18:52 +0300

    Initializing State from Context

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Context } from '../context/BlogContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
-
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam('id')
   );
@@ -12,10 +13,31 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Edit', { id: navigation.getParam('id') })
+        }
+      >
+        <FontAwesome style={styles.plusIncon} name='pencil-square-o' />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  plusIncon: {
+    fontSize: 30,
+    marginHorizontal: 10,
+    color: 'black',
+  },
+});
 
 export default ShowScreen;
