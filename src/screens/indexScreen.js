d3752fa84ff145354aeb4e8695a986df596eac2f
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 10:32:04 +0300

    Adding State in Context

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from './context/BlogContext';
 
 const indexScreen = () => {
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
 
   return (
     <View>
-      <Text>Value: {value}</Text>
+      <FlatList
+        keyExtractor={(blogPost) => blogPost.title}
+        data={blogPosts}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
